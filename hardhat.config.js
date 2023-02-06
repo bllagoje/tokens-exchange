73c2a30972554c2d402486d83e3de743907336f0
blagoje <bllagoje@gmail.com>
2023-02-06 23:00:24 +0100

    Initial Commit

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+// require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+};
+
+
+// add
+// @nomicfoundation/hardhat-toolbox
+// @nomiclabs/hardhat-ethers
