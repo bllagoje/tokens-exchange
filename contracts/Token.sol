9ea01f6457ca5f18e7099209966081886cdaaff8
blagoje <bllagoje@gmail.com>
2023-02-08 16:05:14 +0100

    Update Contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -8,14 +8,19 @@ contract Token {
     string public symbol;
     uint public decimals = 18;
     uint public totalSupply;
+
+    mapping (address => uint) public balanceOf;
+
     
     constructor(string memory _name, string memory _symbol, uint _totalSupply) {
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply * (10 ** decimals);
+        balanceOf[msg.sender] = totalSupply;
     }
 
     
+
 
 }
 
--- test/Token.js
+++ test/Token.js
@@ -7,11 +7,15 @@ const tokens = (n) => {
 
 describe("Token Contract", () => {
     let token
+    let accounts
+    let deployer
 
     beforeEach(async () => {
         // Fetch Token from Blockchain:
         let Token = await ethers.getContractFactory("Token")
         token = await Token.deploy("Dapp University", "DAPP", "1000000")
+        accounts = await ethers.getSigners()
+        deployer = accounts[0]
     })
 
     describe("Deployment", () => {
@@ -19,6 +23,7 @@ describe("Token Contract", () => {
         const symbolInput = "DAPP"
         const decimalsInput = "18"
         const valueInput = tokens("1000000")
+        
 
         it("Has correct name", async () => {
             let name = await token.name()
@@ -38,6 +43,11 @@ describe("Token Contract", () => {
         it("Has correct total supply", async () => {
             let totalSupply = await token.totalSupply()
             expect(totalSupply).to.equal(valueInput)
+        })
+
+        it("Assigns total supply to deployer", async () => {
+            let balanceOfDeployer = await token.balanceOf(deployer.address)
+            expect(balanceOfDeployer).to.equal(valueInput)
         })
 
     })
