eac6b9a1828f09f4acf5cdcf83c5cd6f8d10d1ee
blagoje <bllagoje@gmail.com>
2023-02-27 13:42:23 +0100

    Add Order Book

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import config from "../config.json"
 
-import { loadProvider, loadNetwork, loadAccount, loadTokens, loadExchange, subscribeToEvents } from "../store/interactions"
+import { loadProvider, loadNetwork, loadAccount, loadTokens, loadExchange, loadAllOrders, subscribeToEvents } from "../store/interactions"
 import Navbar from "./Navbar"
 import Markets from "./Markets"
 import Balance from "./Balance"
 import Order from "./Order"
+import OrderBook from "./OrderBook"
 
 
 function App() {
@@ -39,6 +40,9 @@ function App() {
     const exchangeConfig = config[chainId].exchange
     const exchange = await loadExchange(provider, exchangeConfig.address, dispatch)
 
+    // Fetch all orders: open, filled, cancelled
+    loadAllOrders(provider, exchange, dispatch)
+    
     // Listen to events
     subscribeToEvents(exchange, dispatch)
   }
@@ -70,7 +74,7 @@ function App() {
 
           {/* Trades */}
 
-          {/* OrderBook */}
+          <OrderBook />
 
         </section>
       </main>
